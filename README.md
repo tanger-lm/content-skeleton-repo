8f3c8183443d711fe8aa410197b45fc77d666f5f
tanger-lm <eric.tang@lemon.markets>
2022-06-22 11:59:02 +0200

    updated to conform with video content

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # content-skeleton-repo
+
+A repository with all the basic structure a lemon.markets project needs.
--- main.py
+++ main.py
@@ -4,12 +4,11 @@ from datetime import datetime
 from dotenv import load_dotenv
 from lemon import api
 
-
 load_dotenv()
 client = api.create(
     trading_api_token=os.environ.get('TRADING_API_KEY'),
     market_data_api_token=os.environ.get('DATA_API_KEY'),
-    env='paper'
+    env='paper'  # or 'money'
 )
 print(client)
 
@@ -18,47 +17,15 @@ def demo_data():
     # get venues
     response = client.market_data.venues.get()
     print(response.results)
-
-    # get instruments
-    response = client.market_data.instruments.get(
-        isin=["US88160R1014", "US0231351067"]
-    )
-    print(response.results)
-
-    response = client.market_data.instruments.get(
-        type=['stock', 'etf'],
-        currency=['EUR'],
-        limit=10,
-        sorting='asc'
-    )
-    print(response.results)
+    print('\n')
 
 
 def demo_trading():
-
     # activate buy order
-    response = client.trading.orders.create(
-        isin='US88160R1014',
-        side='buy',
-        quantity=1,
-        venue='xmun'
-    )
-    order_id = response.results.id
+    response = client.trading.account.get()
     print(response.results)
-    response = client.trading.orders.activate(order_id=order_id)
-    print(response)
-
-    # get buy order status
-    response = client.trading.orders.get_order(order_id=order_id)
-    print(response.results)
-
-    # cancel order
-    response = client.trading.orders.create(isin='US88160R1014', side='buy', quantity=1, venue='xmun')
-    print(response.results)
-    response = client.trading.orders.cancel(order_id=response.results.id)
-    print(response)
 
 
 if __name__ == '__main__':
     demo_data()
-    # demo_trading()
+    demo_trading()
