0b2d2c6ba3d6d734bd3b02179cc11d735d9e801a
tanger-lm <eric.tang@lemon.markets>
2022-06-23 16:22:29 +0200

    reformat for video content

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ load_dotenv()
 # create your client
 client = api.create(
     trading_api_token=os.environ.get('TRADING_API_KEY'),
-    market_data_api_token=os.environ.get('DATA_API_KEY'),
+    market_data_api_token=os.environ.get('MARKET_DATA_API_KEY'),
     env='paper'  # or 'money'
 )
-print(client)
 
 
 def demo_data():
@@ -21,26 +20,26 @@ def demo_data():
     print('\n')
 
 
-def demo_trading():
-    # get account details
-    response = client.trading.account.get()
-    print(response.results)
-
-    # activate buy order
-    response = client.trading.orders.create(isin='US88160R1014', side='buy', quantity=1, venue='xmun')
-    order_id = response.results.id
-    print(response.results)
-    response = client.trading.orders.activate(order_id=order_id)
-    print(response)
-
-    # cancel order
-    response = client.trading.orders.create(isin='US88160R1014', side='buy', quantity=1, venue='xmun')
-    order_id = response.results.id
-    print(response.results)
-    response = client.trading.orders.cancel(order_id=order_id)
-    print(response)
-
-
+# def demo_trading():
+#     # get account details
+#     response = client.trading.account.get()
+#     print(response.results)
+#
+#     # activate buy order
+#     response = client.trading.orders.create(isin='US88160R1014', side='buy', quantity=1, venue='xmun')
+#     order_id = response.results.id
+#     print(response.results)
+#     response = client.trading.orders.activate(order_id=order_id)
+#     print(response)
+#
+#     # cancel order
+#     response = client.trading.orders.create(isin='US88160R1014', side='buy', quantity=1, venue='xmun')
+#     order_id = response.results.id
+#     print(response.results)
+#     response = client.trading.orders.cancel(order_id=order_id)
+#     print(response)
+#
+#
 if __name__ == '__main__':
     demo_data()
-    demo_trading()
+#     demo_trading()
