6f2d58326ec5e3a4af99604d1396763015851463
SirGnat <eric_tang@brown.edu>
2022-06-24 16:40:49 +0200

    update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 from dotenv import load_dotenv
 from lemon import api
@@ -8,7 +9,7 @@ load_dotenv()
 # create your client
 client = api.create(
     trading_api_token=os.environ.get('TRADING_API_KEY'),
-    market_data_api_token=os.environ.get('MARKET_DATA_API_KEY'),
+    market_data_api_token=os.environ.get('DATA_API_KEY'),
     env='paper'  # or 'money'
 )
 
@@ -20,26 +21,34 @@ def demo_data():
     print('\n')
 
 
-# def demo_trading():
-#     # get account details
-#     response = client.trading.account.get()
-#     print(response.results)
-#
-#     # activate buy order
-#     response = client.trading.orders.create(isin='US88160R1014', side='buy', quantity=1, venue='xmun')
-#     order_id = response.results.id
-#     print(response.results)
-#     response = client.trading.orders.activate(order_id=order_id)
-#     print(response)
-#
-#     # cancel order
-#     response = client.trading.orders.create(isin='US88160R1014', side='buy', quantity=1, venue='xmun')
-#     order_id = response.results.id
-#     print(response.results)
-#     response = client.trading.orders.cancel(order_id=order_id)
-#     print(response)
-#
-#
+def demo_trading():
+    # get account details
+    response = client.trading.account.get()
+    print(response.results)
+
+    # activate buy order
+    response = client.trading.orders.create(isin='US88160R1014', side='buy', quantity=1, venue=os.getenv('MIC'))
+    order_id = response.results.id
+    print(response.results)
+    response = client.trading.orders.activate(order_id=order_id)
+    print(response)
+
+    # cancel order
+    response = client.trading.orders.create(
+        isin='US88160R1014',
+        side='buy',
+        quantity=1,
+        venue=os.getenv('MIC'),
+        stop_price=6810000,
+        limit_price=6850000,
+        expires_at=datetime(2022, 7, 1)
+    )
+    order_id = response.results.id
+    print(response.results)
+    response = client.trading.orders.cancel(order_id=order_id)
+    print(response)
+
+
 if __name__ == '__main__':
     demo_data()
-#     demo_trading()
+    demo_trading()
